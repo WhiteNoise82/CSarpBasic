60617fd396e937beffd5fbbf93f11fddee9ab35e
OH SOUNG KWON <crystals8960@gmail.com>
2019-02-13 19:07:20 +0900

    Accessor Ex

---
+++ Accessor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accessor
+{
+    class Test
+    {
+        private int x;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.X = 13;
+
+            Console.WriteLine(test.X);
+
+        }
+    }
+}
