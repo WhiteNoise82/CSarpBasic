6ef21d2ff107c6aafe0c11be241f58ad6e8d5b6f
OH SOUNG KWON <crystals8960@gmail.com>
2019-02-13 17:32:48 +0900

    AbstractClass

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    abstract class 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
