36868d2db0dcd7b3937144d61e445188a2991bbd
OH SOUNG KWON <crystals8960@gmail.com>
2019-02-15 00:17:28 +0900

    ArrayEx1

---
+++ Array1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] locker = new int[5];
+            int[,] nums = new int[3, 2] { { 9, 99 }, { 3, 33 }, { 5, 55 } };
+
+            for(int i = 0; i < locker.Length; i++)
+            {
+                locker[i] = i + 1;
+            }
+
+            for (int i = 0; i < locker.Length; i++)
+            {
+                Console.Write("{0} ", locker[i]);
+            }
+            Console.WriteLine();
+
+            foreach(int loc in locker)
+            {
+                Console.Write("{0} ", loc);
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", nums[i, j]);
+                }
+            }
+            Console.WriteLine();
+
+            foreach (int i in nums)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
