bd58aa27a8092c42c51ccf08d572148ad399bf8a
OH SOUNG KWON <crystals8960@gmail.com>
2019-02-15 19:13:38 +0900

    Making Forms at Console Program

---
+++ WinForm1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm1
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Button btn1 = new Button();
+            btn1.Text = "Click!!";
+            btn1.Top = 100;
+            btn1.Left = 150;
+
+            Program form1 = new Program();
+            form1.Text = "Button & Form";
+            form1.Width = 400;
+            form1.Height = 300;
+
+            form1.Controls.Add(btn1);
+
+            btn1.Click += (object sender,EventArgs e) =>
+            {
+                MessageBox.Show("메세지 상자입니다.", "모달", MessageBoxButtons.AbortRetryIgnore);
+            };
+
+            Application.Run(form1);
+        }
+    }
+}
