2cd7446257e7cf4201bcbccf6466438162caebfa
OH SOUNG KWON <crystals8960@gmail.com>
2019-02-25 00:20:57 +0900

    Delegatew4Ex Completed

--- Delegate3/Program.cs
+++ Delegate3/Program.cs
@@ -28,12 +28,42 @@ namespace Delegate3
 
             for (i = 0; i < DataSet.Length - 1; i++)
             {
-
+                for (j = 0; j < DataSet.Length - (i + 1); j++)
+                {
+                    if(Comparer(DataSet[j], DataSet[j + 1]) > 0)
+                    {
+                    temp = DataSet[j + 1];
+                    DataSet[j + 1] = DataSet[j];
+                    DataSet[j] = temp;
+                    }
+                }
             }
         }
 
         static void Main(string[] args)
         {
+            int[] array = { 3, 7, 4, 2, 10 };
+
+            Console.WriteLine("Sorting Ascending...");
+            BubbleSort<int>(array, new Compare<int>(AscendCompare));
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+
+            string[] array2 = { "abc", "def", "ghi", "jkl", "mno" };
+
+            Console.WriteLine("\nSorting descending...");
+            BubbleSort<string>(array2, new Compare<string>(DescendCompare));
+
+            for(int i = 0; i < array2.Length; i++)
+            {
+                Console.Write($"{array2[i]} ");
+                               
+            }
+
+            Console.WriteLine();
         }
     }
 }
---
+++ Delegate4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate4
+{
+    delegate void Notify(string message);
+    class Notifier
+    {
+        public Notify EventOccured;
+    }
+
+    class EventListener
+    {
+        private string name;
+        public EventListener(string name)
+        {
+            this.name = name;
+        }
+
+        public void SomethingHappened(string message)
+        {
+            Console.WriteLine($"{name}.SomethingHappened : {message}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notifier notifier = new Notifier();
+            EventListener listener1 = new EventListener("Listener1");
+            EventListener listener2 = new EventListener("Listener2");
+            EventListener listener3 = new EventListener("Listener3");
+
+            notifier.EventOccured += listener1.SomethingHappened;
+            notifier.EventOccured += listener2.SomethingHappened;
+            notifier.EventOccured += listener3.SomethingHappened;
+            notifier.EventOccured("You've got mail.");
+
+            Console.WriteLine();
+
+            notifier.EventOccured -= listener2.SomethingHappened;
+            notifier.EventOccured("Download complete.");
+
+            Console.WriteLine();
+
+            Notify notify1 = new Notify(listener1.SomethingHappened);
+            Notify notify2 = new Notify(listener2.SomethingHappened);
+
+            notifier.EventOccured = (Notify)Delegate.Combine(notify1, notify2);
+            notifier.EventOccured("Fire!!");
+
+            Console.WriteLine();
+
+            notifier.EventOccured = (Notify)Delegate.Remove(notifier.EventOccured, notify2);
+            notifier.EventOccured("RPG!");
+
+
+        }
+    }
+}
