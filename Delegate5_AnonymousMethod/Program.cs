8f388f749c95559515534092fd464be6da871505
OH SOUNG KWON <crystals8960@gmail.com>
2019-03-18 02:06:23 +0900

    AnonymousMethod

---
+++ Delegate5_AnonymousMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate5_AnonymousMethod
+{
+    delegate int Compare(int a, int b);
+
+    class Program
+    {
+        static void BubbleSort(int[] DataSet, Compare Comparer)
+        {
+            int i = 0;
+            int j = 0;
+            int temp = 0;
+
+            for (i = 0; i < DataSet.Length - 1; i++)
+            {
+                for (j = 0; j < DataSet.Length - (i + 1); j++)
+                {
+                    if (Comparer(DataSet[j], DataSet[j + 1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 2, 10 };
+
+            Console.WriteLine("Sorting ascending...");
+            BubbleSort(array, delegate (int a, int b)
+            {
+                if (a > b)
+                    return 1;
+                else if (a == b)
+                    return 0;
+        }
+    }
+}
